4fee604dca48021e57ce3a6c093d65025a80cffe
Daniel Luu <lnhhd2000@gmail.com>
2024-09-16 18:56:01 +1000

    Remove Navbar

--- src/sections/Navbar/Navbar.jsx
+++ src/sections/Navbar/Navbar.jsx
@@ -3,7 +3,7 @@ import styles from "./Navbar.module.css";
 
 const Navbar = () => {
   return (
-    <div className="styles.container">Navbar</div>
+    <div className="styles.container"></div>
   )
 }
 
